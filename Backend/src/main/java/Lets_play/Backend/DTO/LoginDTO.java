f23a199e347e376b233fb0e455abe350f765ba35
Ayamne <ouchchateaymane@gmail.cpm>
2026-01-29 09:50:27 +0100

    fix db issues

--- Backend/src/main/java/Lets_play/Backend/DTO/LoginDTO.java
+++ Backend/src/main/java/Lets_play/Backend/DTO/LoginDTO.java
@@ -3,11 +3,14 @@ package Lets_play.Backend.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginDTO {
+    @NonNull
     private String username;
+    @NonNull
     private String password;
 }
--- Backend/src/main/java/Lets_play/Backend/DTO/RegisterDTO.java
+++ Backend/src/main/java/Lets_play/Backend/DTO/RegisterDTO.java
@@ -3,12 +3,18 @@ package Lets_play.Backend.DTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterDTO {
+    @NonNull
     private String username;
+
+    @NonNull
     private String email;
+
+    @NonNull
     private String password;
 }
